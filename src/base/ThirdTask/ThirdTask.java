fbfbf4e7d07d792361cd0b5dd9ecca55920dadcf
Reinhard <deuxexmachina1973@gmail.com>
2026-06-15 16:25:53 +0300

    Add third task.

---
+++ src/base/ThirdTask/ThirdTask.java
@@ -0,0 +1,22 @@
+package base.ThirdTask;
+
+public class ThirdTask {
+    public static void main(String[] args) {
+        System.out.println("Дмитрий");
+        System.out.println(123 * 456);
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.println("*");
+        System.out.print("Первый");
+        System.out.print("Второй");
+        System.out.print("Третий");
+        System.out.print("Четвертый");
+    }
+}
