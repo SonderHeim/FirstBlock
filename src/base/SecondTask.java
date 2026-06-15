6d162eca7562069aea3711b90f0f9548611a00d5
Reinhard <deuxexmachina1973@gmail.com>
2026-06-15 15:59:02 +0300

    Add second task.

---
+++ src/base/SecondTask.java
@@ -0,0 +1,9 @@
+package base;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        System.out.println("Дмитрий");
+        System.out.println(25);
+        System.out.println("Java");
+    }
+}
