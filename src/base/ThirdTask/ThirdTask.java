59e03c7edd8fea21fe6215f126257188a4d928ce
Reinhard <deuxexmachina1973@gmail.com>
2026-06-28 22:12:48 +0300

    Update project v1.1.

--- src/base/ThirdTask/ThirdTask.java
+++ src/base/ThirdTask/ThirdTask.java
@@ -4,19 +4,10 @@ public class ThirdTask {
     public static void main(String[] args) {
         System.out.println("Дмитрий");
         System.out.println(123 * 456);
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.println("*");
-        System.out.print("Первый");
-        System.out.print("Второй");
-        System.out.print("Третий");
+        System.out.print("*".repeat(10));
+        System.out.print("Первый ");
+        System.out.print("Второй ");
+        System.out.print("Третий ");
         System.out.print("Четвертый");
     }
 }
